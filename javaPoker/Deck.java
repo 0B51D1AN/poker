ee7f7d6593689a07d8056b05458d39c22aec72bf
Kevin D. Rodriguez <rodriguez_kd@cobra.cs.mercer.edu>
2023-09-05 10:44:30 -0400

    Intro to Deck.java

---
+++ javaPoker/Deck.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Deck extends Card
+{
+	//Deck class extends card and assembles card objects into deck. 
+	//Methods needed:
+	//Shuffle
+	//Deal
+	//Remove Card (popCard)
+	//Add Card (pushCard)
+	//Sort
+
+	private List<Deck> deck;
+
+	//Need to check to make sure all cards in a deck are placed inside of the deck object.
+	//
+	//
+	public Deck()
+	{
+		
+		deck= new ArrayList<Deck>(); //Perhaps change to arraylist later
+							// Fill Deck with Cards
+		
+				
+				
+	}
+	
+
+	
+	
+
+
+
+
+}
