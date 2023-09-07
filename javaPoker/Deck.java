d99c8b60273823fbeb40ee17900e2442c1a47a23
Kevin D. Rodriguez <rodriguez_kd@cobra.cs.mercer.edu>
2023-09-07 10:48:57 -0400

    deck initialization

--- javaPoker/Deck.java
+++ javaPoker/Deck.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.util.Collections;
 import java.util.List;
-
+//import java.util.String;
 
 public class Deck extends Card
 {
@@ -12,8 +12,8 @@ public class Deck extends Card
 	//Remove Card (popCard)
 	//Add Card (pushCard)
 	//Sort
-
-	private List<Deck> deck;
+	//Display remaining cards
+	private List<Card> deck;
 
 	//Need to check to make sure all cards in a deck are placed inside of the deck object.
 	//
@@ -21,10 +21,38 @@ public class Deck extends Card
 	public Deck()
 	{
 		
-		deck= new ArrayList<Deck>(); //Perhaps change to arraylist later
+		deck= new ArrayList<Card>(); //Perhaps change to arraylist later
 							// Fill Deck with Cards
-		
+		for(int s=1; s<5; s++)
+		{
+			for(int f= 1; f<14; f++)	
+			{
+			String temp="";
+				if(f>10)
+				{
+					switch (f){
+					case 11: temp="J";
+					case 12: temp="Q";
+					case 13: temp="K";			
+					}
+				}
+				else
+				{
+					temp= String.valueOf(f);
+				}
 				
+				
+				switch(s){
+				case 1:	deck.add(new Card(temp,"H"));
+				case 2: deck.add(new Card(temp,"D"));
+				case 3: deck.add(new Card(temp,"S"));
+				case 4: deck.add(new Card(temp,"C"));
+				default: break;
+				}
+
+			}
+
+		}		
 				
 	}
 	
